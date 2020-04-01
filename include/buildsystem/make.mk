3240b4a961b21d5a4eed7211bae64b37080e9a9c
Scott Court <z5t1@z5t1.com>
2020-04-01 11:13:09 -0400

    Added make buildsystem
    This buildsystem handles building packages that use a traditional Makefile,
    without the autoconf ./configure script.

---
+++ include/buildsystem/make.mk
@@ -0,0 +1,34 @@
+include buildconfig.mk
+include package-common.mk
+
+_MAKE_HELPER_TARGETS=\
+	make-preconfigure \
+	make-premake \
+	make-preinstall \
+	make-prepackage
+
+.PHONY: $(_make_HELPER_TARGETS)
+
+.stamp_build: pkg_build_prepare
+	make make-premake
+	cd pkg_src/$(PKG_SRC_DIR); \
+		make $(MAKE_ARGS) \
+		-j $(MAKE_MAXJOBS) \
+		-l $(MAKE_MAXLOAD)
+	make make-preinstall
+	cd pkg_src/$(PKG_SRC_DIR); \
+		make $(MAKE_INSTALL_ARGS) install DESTDIR=$(PKG_ROOT)
+	make make-prepackage
+	touch $@
+
+# Commands to run before make
+make-premake-default:
+
+# Commands to run after make, but before make install
+make-preinstall-default:
+
+# Commands to run after make install, but before the final package is created
+make-prepackage-default:
+
+make-%: make-%-default
+
