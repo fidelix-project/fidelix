7da45f3530100b3ccff732c909c53cb9162f811f
Scott Court <z5t1@z5t1.com>
2020-04-03 09:42:41 -0400

    Fixed directory inclusion in dir.mk

--- include/dir.mk
+++ include/dir.mk
@@ -1,74 +1,84 @@
 ifndef _INC_DIR
-_INC_DIR:=included
+_INC_DIR:=y
 
 include sysconfig.mk
 
-DIR_TARGETS:=$(PACKAGES) $(SUBDIRS)
+DEFAULT_PACKAGES?=$(PACKAGES)
+DIR_TARGETS:=$(DEFAULT_PACKAGES) $(SUBDIRS)
 
 .PHONY: build
 build: $(addprefix build-,$(DIR_TARGETS))
 
 .PHONY: build-%
 build-%:
-	make -C $*
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*)
 
 .PHONY: tidy-%
 tidy-%:
-	make -C $* tidy
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) tidy
 
 .PHONY: tidy
 tidy: $(addprefix tidy-,$(DIR_TARGETS))
 
 .PHONY: neat-%
 neat-%:
-	make -C $* neat
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) neat
 
 .PHONY: neat
 neat: $(addprefix neat-,$(DIR_TARGETS))
 
 .PHONY: scrub-%
 scrub-%:
-	make -C $* scrub
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) scrub
 
 .PHONY: scrub
 scrub: $(addprefix scrub-,$(DIR_TARGETS))
 
 .PHONY: clean-%
 clean-%:
-	make -C $* clean
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) clean
 
 .PHONY: clean
 clean: $(addprefix clean-,$(DIR_TARGETS))
 
 .PHONY: download-%
 download-%:
-	make -C $* download
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) download
 
 .PHONY: download
 download: $(addprefix download-,$(DIR_TARGETS))
 
 .PHONY: verify-%
 verify-%:
-	make -C $* verify
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) verify
 
 .PHONY: verify
 verify: $(addprefix verify-,$(DIR_TARGETS))
 
 .PHONY: install-%
 install-%:
-	make -C $* install
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) install
 
 .PHONY: install
 install: $(addprefix install-,$(DIR_TARGETS))
 
 .PHONY: reinstall-%
 reinstall-%:
-	make -C $* reinstall
+	@[ ! -z "$(DIR_PKG_$*)" ] || (echo $*: package not found && exit 1)
+	make -C $(DIR_PKG_$*) reinstall
 
 .PHONY: reinstall
 reinstall: $(addprefix reinstall-,$(DIR_TARGETS))
 
-undefine PACKAGES
+#undefine PACKAGES
 
 endif
 
@@ -76,46 +86,11 @@ ifndef DIR_INC_$(DIR)
 DIR_INC_$(DIR)=included
 
 ifdef PACKAGES
-
-.PHONY: $(addprefix clean-,$(PACKAGES))
-$(addprefix clean-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix tidy-,$(PACKAGES))
-$(addprefix tidy-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix neat-,$(PACKAGES))
-$(addprefix neat-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix scrub-,$(PACKAGES))
-$(addprefix scrub-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix download-,$(PACKAGES))
-$(addprefix download-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix verify-,$(PACKAGES))
-$(addprefix verify-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix build-,$(PACKAGES))
-$(addprefix build-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix install-,$(PACKAGES))
-$(addprefix install-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
-.PHONY: $(addprefix reinstall-,$(PACKAGES))
-$(addprefix reinstall-,$(PACKAGES)):
-	make -C $(OS_SRC_DIR)/$(DIR) $@
-
+$(foreach pkg,$(PACKAGES),$(eval DIR_PKG_$(pkg):=$(OS_SRC_DIR)/$(DIR)/$(pkg)))
 endif
 
 ifdef SUBDIRS
+$(foreach pkg,$(SUBDIRS),$(eval DIR_PKG_$(pkg):=$(OS_SRC_DIR)/$(DIR)/$(pkg)))
 include $(addsuffix /Makefile,$(SUBDIRS))
 endif
 
