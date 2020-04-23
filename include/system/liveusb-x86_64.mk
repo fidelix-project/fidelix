057d38d961bffaabd1c31049c2a906c589ee6f50
Scott Court <z5t1@z5t1.com>
2020-04-23 14:54:34 -0400

    Added liveusb-x86_64 SYSTEM type

---
+++ include/system/liveusb-x86_64.mk
@@ -0,0 +1,10 @@
+# Live USB x86_64 System Configuration
+#
+# This system configuration should be used for creating bootable
+# live USB/SD card images.
+
+# The packages in the boot group to build for this system
+BOOT_PACKAGES=linux-generic linux-firmware grub boot-scripts fstab  
+# The bootloader to install
+BOOT_LOADER=grub-liveusb
+
