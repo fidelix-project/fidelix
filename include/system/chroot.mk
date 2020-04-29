8cbc42a031b3f9ab026d670384de82403eba3e38
Scott Court <z5t1@z5t1.com>
2020-04-29 16:36:51 -0400

    Added chroot system target

---
+++ include/system/chroot.mk
@@ -0,0 +1,9 @@
+# Chroot System Configuration (All Architectures)
+#
+# This system configuration is intended for use in a
+# chroot/container/docker image. It installs only the userspace and skips all
+# boot related packages.
+
+# The packages in the boot group to build for this system
+BOOT_PACKAGES=
+
