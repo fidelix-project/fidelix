48cd41dc02d9d2ab124eacdde3249d7b27df8129
Scott Court <z5t1@z5t1.com>
2020-04-29 16:37:02 -0400

    Added minimal package selection

---
+++ include/selection/minimal.mk
@@ -0,0 +1,24 @@
+# Package selection for a minimal functional system. Includes only what is
+# necessary to run Fidelix. You will likely want more packages for it to be
+# useful.
+#
+# It is capable of performing a `make install` from /usr/src if the binary
+# packages are already there.
+
+SELECTED_PACKAGES=\
+	aaa-directory-skel \
+        aaa-file-skel \
+        linux-headers \
+        pkgtools \
+        musl \
+        zlib \
+        libstdcxx \
+        libressl \
+        system-scripts \
+        busybox \
+        make \
+        zoneinfo \
+        ncurses \
+	boot \
+	libgcc
+
