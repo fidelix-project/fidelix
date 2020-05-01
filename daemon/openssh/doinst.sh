e9fbbb65232062d89f7f7cf8306f540179046357
Scott Court <z5t1@z5t1.com>
2020-05-01 15:02:42 -0400

    openssh 8.2p1 build 2
    Fixed a bug in doinst.sh where the _sshd user and group were not added.

--- daemon/openssh/doinst.sh
+++ daemon/openssh/doinst.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Add the _sshd user if it does not exist
-[ -n "$(grep '^_sshd:' etc/passwd)" ] && \
+[ -z "$(grep '^_sshd:' etc/passwd)" ] && \
 	echo '_sshd:x:22:22:OpenSSH Privsep User:/var/empty:/bin/false' \
 	>> etc/passwd
 
 # Add the _sshd group if it does not exist
-[ -n "$(grep '^_sshd:' etc/group)" ] && \
+[ -z "$(grep '^_sshd:' etc/group)" ] && \
 	echo '_sshd:x:22:' >> etc/group
 
